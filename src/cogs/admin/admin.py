fae1b540e9c8414b834a2d8b8ab36a4f295400af
Coschda <mikail.doumi@gmail.com>
2022-09-17 09:04:48 +0200

    unban command / unban error / fix in error handling functions

--- src/cogs/admin/admin.py
+++ src/cogs/admin/admin.py
@@ -18,6 +18,8 @@ class Admin(commands.Cog):
     async def kick_err(self, interaction : Interaction, error):
         if isinstance(error, nextcord.errors.Forbidden):
             await interaction.send("I or you don't have the required permissions.", ephemeral=True)
+        else:
+            raise error
 
     @nextcord.slash_command(name="ban", description="Bans a specified member.")
     @commands.has_permissions(ban_members = True)
@@ -29,6 +31,21 @@ class Admin(commands.Cog):
     async def ban_err(self, interaction: Interaction, error):
         if isinstance(error, nextcord.errors.Forbidden):
             await interaction.send("I or you don't have the required permissions.", ephemeral=True)
+        else:
+            raise error
+
+    @nextcord.slash_command(name="ban", description="Unbans a specified member.")
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, interaction : Interaction, member : nextcord.Member = SlashOption(description="Member to unban", required=True), *, reason=None):
+        await member.unban(reason = reason)
+        await interaction.send(f'{member} a été unban.')
+
+    @unban.error
+    async def unban_err(self, interaction: Interaction, error):
+        if isinstance(error, nextcord.errors.Forbidden):
+            await interaction.send("I or you don't have the required permissions.", ephemeral=True)
+        else:
+            raise error
 
 # .........................[setup function]....................
 def setup(client):
