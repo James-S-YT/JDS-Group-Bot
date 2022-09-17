81fe1567acd0a7a66e9d55b842deb89eced11a6e
Coschda <mikail.doumi@gmail.com>
2022-09-17 11:20:12 +0200

    tempban

--- src/cogs/admin/admin.py
+++ src/cogs/admin/admin.py
@@ -1,8 +1,32 @@
 # .........................[imports]............................
 import nextcord
 import datetime
+import asyncio
 from nextcord import Interaction, SlashOption
 from nextcord.ext import commands
+
+def time2sec(time):
+    # There is actually a library to make this whole thing easier, humanfriendly, but it would add a dependency.
+    mult = {
+        'ms': 0.001, 
+        's': 1, 
+        'm': 60, 
+        'h': 3600, 
+        'd': 86400, 
+        'w': 604800, 
+        'y': 31564000, 
+    }
+
+    nb = ''
+    unit = ''
+    for i in time:
+        try:
+            int(i)
+            nb += i
+        except:
+                unit += i
+    ftime = float(int(nb)*mult[unit])
+    return ftime 
 
 # .........................[admin class]......................
 class Admin(commands.Cog):
@@ -48,31 +72,12 @@ class Admin(commands.Cog):
         else:
             raise error
 
-    @nextcord.slash_command(name="mute", description="Mutes a specified member.")
+    @nextcord.slash_command(name="timeout", description="timeouts a specified member.")
     @commands.has_permissions(moderate_members=True)
-    async def timeout(self, interaction : Interaction, member : nextcord.Member = SlashOption(description="Member to mute", required=True), time : str = SlashOption(description = "Duration of the mute. (50s, 4h, 7d...)", required=True) ,*, reason=None):
-        # There is actually a library to make this whole thing easier, humanfriendly, but it would add a dependency.
-        mult = {
-            'ms': 0.001, 
-            's': 1, 
-            'm': 60, 
-            'h': 3600, 
-            'd': 86400, 
-            'w': 604800, 
-            'y': 31564000, 
-        }
-
-        nb = ''
-        unit = ''
-        for i in time:
-            try:
-                int(i)
-                nb += i
-            except:
-                unit += i
-        ftime = float(int(nb)*mult[unit])
+    async def timeout(self, interaction : Interaction, member : nextcord.Member = SlashOption(description="Member to timeout", required=True), time : str = SlashOption(description = "Duration of the timeout. (50s, 4h, 7d...)", required=True) ,*, reason=None):
+        ftime = time2sec(time)
         await member.timeout(timeout = nextcord.utils.utcnow() + datetime.timedelta(seconds = ftime), reason = reason)
-        await interaction.send(f'{member} has been muted for {time}.')
+        await interaction.send(f'{member} has been timeouted for {time}.')
 
     @timeout.error
     async def timeout_err(self, interaction: Interaction, error):
@@ -94,6 +99,21 @@ class Admin(commands.Cog):
         else:
             raise error
 
+    @nextcord.slash_command(name="tempban", description="Ban temporairement un membre du serveur", guild_ids=ids)
+    @commands.has_permissions(ban_members=True)
+    async def tempban(self, interaction : Interaction, member : nextcord.Member = SlashOption(description="Member to unban", required=True),time : str = SlashOption(description = "Duration of the timeout. (50s, 4h, 7d...)", required=True), *, reason=None):
+        await member.ban(reason = reason)
+        await interaction.send(f'{member} a été ban.')
+        await asyncio.sleep(time2sec(time))
+        await member.unban()
+        await interaction.send(f'{member} a été unban.')
+
+    @tempban.error
+    async def tempban_err(self, interaction: Interaction, error):
+        if isinstance(error, nextcord.errors.Forbidden):
+            await interaction.send("Vous ou je n'ai pas les permissions requises.", ephemeral=True)
+        else:
+            raise error
 
 # .........................[setup function]....................
 def setup(client):
