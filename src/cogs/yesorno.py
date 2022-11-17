a3e76fbbfc87c669394e9c828a2da83e1a5db1eb
Ahmed <93637332+Ahmed3457@users.noreply.github.com>
2022-11-17 23:08:45 +0300

    quick fix

--- src/cogs/yesorno.py
+++ src/cogs/yesorno.py
@@ -1,22 +1,17 @@
 import nextcord,sys
 from nextcord.ext import commands
 from random import choice, shuffle
-sys.path.insert(1, 'cogs\lib')
-import values as v
-
-guilds=v.values.getData("guilds")
-embedColor=v.values.getData("color")
 
 class YesOrNo(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @nextcord.slash_command(guild_ids=guilds, description="Yes Or No Command Ask the bot a question")
+    @nextcord.slash_command(description="Yes Or No Command Ask the bot a question")
     async def yesorno(self, interaction: nextcord.Interaction, question: str = nextcord.SlashOption(description="Question", required=True)):
         await interaction.response.defer()
         answers = ["Yes", "No", "Of Course", "Of Course ||No||", "I Can't Decide","是的，但是中文","不"]
         shuffle(answers)
-        Embed = nextcord.Embed(title=question, description=f"{choice(answers)}", color=embedColor)
+        Embed = nextcord.Embed(title=question, description=f"{choice(answers)}", color=0xc9271e)
         await interaction.followup.send(embed=Embed)
 
 # Setup
